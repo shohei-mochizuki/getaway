a4c2cbe1d618c934c8b6a9f3ea732ac57dac8fb5
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-03-30 14:26:01 -0400

    Added *

--- server/server.js
+++ server/server.js
@@ -21,11 +21,11 @@ app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/public')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/public/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 
