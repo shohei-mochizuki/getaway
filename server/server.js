f7e25670dff7829ac2e6ef2c70e0d875a10cd188
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-03-30 14:30:37 -0400

    Added *

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // TEST
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
