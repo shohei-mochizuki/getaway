d144cd901a341a782ed3678315f2651f424b48b1
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-03-30 14:28:03 -0400

    Added test comment

--- server/server.js
+++ server/server.js
@@ -24,6 +24,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+// TEST
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
