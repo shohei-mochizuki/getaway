ba1feed3bd8c7b7a8f07ecefef773ce464a27c8b
swmac <shaynefw@gmail.com>
2023-03-31 10:02:32 -0400

    style: Add custom CSS to improve the look of Signup and Login pages

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { Link } from 'react-router-dom';
-import { LOGIN } from '../utils/mutations';
-import Auth from '../utils/auth';
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { Link } from "react-router-dom";
+import { LOGIN } from "../utils/mutations";
+import Auth from "../utils/auth";
+import "./assets/css/login.css";
 
 function Login(props) {
-  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [formState, setFormState] = useState({ email: "", password: "" });
   const [login, { error }] = useMutation(LOGIN);
 
   const handleFormSubmit = async (event) => {
@@ -31,37 +32,51 @@ function Login(props) {
 
   return (
     <div className="container my-1">
-      <Link to="/signup">← Go to Signup</Link>
+      <Link to="/signup" className="back-link">
+        ← Don't have an account? Go to Signup!
+      </Link>
 
-      <h2>Login</h2>
+      <h2 className="form-title">Login</h2>
+
       <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email address:</label>
+        <div className="form-group">
+          <label htmlFor="email" className="form-label">
+            Email address:
+          </label>
           <input
             placeholder="youremail@test.com"
             name="email"
             type="email"
             id="email"
             onChange={handleChange}
+            className="form-control"
           />
         </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
+
+        <div className="form-group">
+          <label htmlFor="pwd" className="form-label">
+            Password:
+          </label>
           <input
             placeholder="******"
             name="password"
             type="password"
             id="pwd"
             onChange={handleChange}
+            className="form-control"
           />
         </div>
+
         {error ? (
           <div>
             <p className="error-text">The provided credentials are incorrect</p>
           </div>
         ) : null}
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
+
+        <div className="form-group">
+          <button type="submit" className="submit-btn">
+            Submit
+          </button>
         </div>
       </form>
     </div>
