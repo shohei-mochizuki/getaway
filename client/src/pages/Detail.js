24253ea34eedf54bf8407d2c5270958e04a95a8b
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-03-31 15:09:54 -0400

    Leave comments to Details.js

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -4,20 +4,17 @@ import { useQuery } from '@apollo/client';
 
 import Cart from '../components/Cart';
 import { useStoreContext } from '../utils/GlobalState';
-import {
-  REMOVE_FROM_CART,
-  UPDATE_CART_QUANTITY,
-  ADD_TO_CART,
-  UPDATE_PRODUCTS,
-} from '../utils/actions';
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY, ADD_TO_CART, UPDATE_PRODUCTS } from '../utils/actions';
 import { QUERY_PRODUCTS } from '../utils/queries';
 import { idbPromise } from '../utils/helpers';
 import spinner from '../assets/spinner.gif';
 
 function Detail() {
+  // Use global state
   const [state, dispatch] = useStoreContext();
   const { id } = useParams();
 
+  // Set up local state in order to solve rendeing issue (global state variables not being updated immediately)
   const [currentProduct, setCurrentProduct] = useState({});
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
